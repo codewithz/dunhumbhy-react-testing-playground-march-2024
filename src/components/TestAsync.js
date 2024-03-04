7c31fda524f6a14dd8aaaf786221475e983324d9
Zartab Nakhwa <zartab@codewithz.com>
2024-03-04 16:12:57 +0530

    Test Router

--- src/components/TestAsync.js
+++ src/components/TestAsync.js
@@ -13,7 +13,8 @@ return (
   <>
     <h1 data-testid="counter">{ counter }</h1>
     <button data-testid="button-up" onClick={delayCount}> Up</button>
-    <button data-testid="button-down" onClick={() => setCounter(counter - 1)}>Down</button>
+    <button data-testid="button-down" onClick={() => setCounter(counter - 1)}>
+      Down</button>
  </>
     )
   }
